30ece287a22098326e560cd6d6bf82acb68cf0df
Paul Peregud <paulperegud@protonmail.com>
2025-09-12 16:52:10 +0200

    bump zig to 0.15.1

--- flake.nix
+++ flake.nix
@@ -6,16 +6,21 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages.${system}; in
-    {
-        devShells.default = pkgs.mkShell {
-            buildInputs = [
-                pkgs.zig
-                pkgs.renameutils # qmv
-                pkgs.coreutils
-                pkgs.tree
-            ];
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.zig_0_15
+          pkgs.renameutils # qmv
+          pkgs.coreutils
+          pkgs.tree
+        ];
+      };
     });
 }
